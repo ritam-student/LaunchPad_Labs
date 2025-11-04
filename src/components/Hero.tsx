9eaffe53cc07f6d49615d92def991e83e95bc78f
ritam-student <ritamacharya2004@gmail.com>
2025-11-04 23:30:31 +0530

    added pricing part-1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,88 +10,88 @@ function Hero() {
                     <div className="flex items-center justify-between gap-6 md:gap-8  lg:gap-12 content">
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden" >
-                                <img src="/public/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                     </div>
                     <div className="flex items-center justify-between gap-6 md:gap-8  lg:gap-12 content">
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden" >
-                                <img src="/public/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                     </div>
                     <div className="flex items-center justify-between gap-6 md:gap-8  lg:gap-12 content">
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Qualcomm-Logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden" >
-                                <img src="/public/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Walmart_logo_(2008) 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Amazon_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Adobe_Corporate_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                         <div className="block">
                             <div className=" w-[115px] h-[28px] flex items-center justify-center overflow-hidden">
-                                <img src="/public/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
+                                <img src="/Deloitte_old_blue_logo 1.png" alt="" className="h-full w-full object-contain" />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className=" h-auto w-[30%] absolute top-[45%] lg:top-[55%] left-[50%] translate-x-[-50%] md:top-[50%] md:translate-y-[-50%] translate-y-[-45%] lg:translate-y-[-55%] pt-8 flex items-center justify-center ">
-                <img src="/public/Book-a-call.png" alt="" />
+                <img src="/Book-a-call.png" alt="" />
             </div>
         </div>
         
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,6 +1,25 @@
+import Button2 from "./ui/Button2";
+
 function Pricing() {
-    return <div>
-        pricing
+    return <div className="px-4 md:px-8 lg:px-14 py-24 bg-[#F3F7F8] ">
+        <div className="h-auto w-full pt-4 md:pt-6 lg:pt-8 flex items-center justify-center">
+            <Button2 text="Pricing" />
+        </div>
+        <div className="h-auto w-full pt-6 flex items-center justify-center">
+            <h2 className="text-[#202020] text-[28px] font-semibold leading-[31px] text-center px-6 ">Two ways to launch fast - and keep growing even faster</h2>
+        </div>
+        <div className="h-auto w-full pt-4 pb-16 flex items-center justify-center px-14 text-center">
+            <p className="text-[16px] leading-[24px] md:leading-[31px] ">Choose the perfect package to launch and grow your startup</p>
+        </div>
+        <div className="w-full h-auto flex flex-col lg:flex-row items-center justify-center gap-2">
+            <div>
+                <img src="/pricing_img2.png" alt="" />
+            </div>
+            <div className="h-auto w-auto relative flex items-center justify-center  ">
+                <img src="/pricing_img1.png" alt="" className="h-full w-full object-contain"/>
+            </div>
+        </div>
+        
     </div>
 }
 
